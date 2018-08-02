f9a3510d81823f8eb07bf43ec47f0fb47e27c69f
于红雨 <yuhongyu04@126.com>
2018-08-02 18:03:00 +0800

    modify woker.main

--- worker/src/main/scala/io/upblockchain/worker/Main.scala
+++ worker/src/main/scala/io/upblockchain/worker/Main.scala
@@ -1,31 +1,35 @@
 package io.upblockchain.worker
 
+import java.util.concurrent.TimeUnit
+
 import io.upblockchain.common._
 import com.google.inject.Guice
-
 import akka.actor.ActorSystem
 import akka.actor.Props
 import akka.cluster.client.ClusterClientReceptionist
 import io.upblockchain.worker.modules.ServiceModule
 import akka.stream.ActorMaterializer
-import io.upblockchain.worker.client.GethClient
+import akka.util.Timeout
+import com.google.protobuf.ByteString
+import com.google.protobuf.any.Any
+import io.upblockchain.common.model.{ JsonRPCRequest, JsonRPCResponse }
+import io.upblockchain.worker.services.StatsMonitor
+
+import scala.collection.mutable
+import scala.util.Success
+//import io.upblockchain.worker.client.GethClient
+import akka.pattern.ask
 
 object Main extends App {
+  import scala.concurrent.ExecutionContext.Implicits.global
 
+  implicit val timeout = Timeout.apply(3, TimeUnit.SECONDS)
   val injector = Guice.createInjector(ServiceModule)
   implicit val system = injector.getInstance(classOf[ActorSystem])
   implicit val mat = injector.getInstance(classOf[ActorMaterializer])
 
-  val serviceA = injector.getActor("GethActor")
-  ClusterClientReceptionist(system).registerService(serviceA)
+  val clientRouter = injector.getActor("ClientRouter")
+  ClusterClientReceptionist(system).registerService(clientRouter)
 
-  println(logo)
-
-  lazy val logo = """
-   _       __           __            
-  | |     / /___  _____/ /_____  _____
-  | | /| / / __ \/ ___/ //_/ _ \/ ___/
-  | |/ |/ / /_/ / /  / ,< /  __/ /    
-  |__/|__/\____/_/  /_/|_|\___/_/     """
-
+  val monitor = system.actorOf(StatsMonitor.props(clientRouter))
 }
