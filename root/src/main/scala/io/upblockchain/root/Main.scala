8ad762c50aa4c2696ede20b2ccc94a6550b399c1
于红雨 <yuhongyu04@126.com>
2018-08-09 12:38:45 +0800

    add root router

--- root/src/main/scala/io/upblockchain/root/Main.scala
+++ root/src/main/scala/io/upblockchain/root/Main.scala
@@ -1,13 +1,14 @@
 package io.upblockchain.root
 
 import com.google.inject.Guice
-
-import akka.actor.ActorSystem
+import akka.actor.{ ActorRef, ActorSystem, Props }
+import akka.cluster.Cluster
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import io.upblockchain.root.modules._
-import io.upblockchain.root.routees.RootRoute
+import io.upblockchain.root.routees.{ RootRoute }
 import com.typesafe.config.Config
+import io.upblockchain.root.router.{ EthWorkerGroup, RootRouter }
 
 object Main extends App {
 
@@ -16,11 +17,11 @@ object Main extends App {
   implicit val system = injector.getInstance(classOf[ActorSystem])
   implicit val mat = injector.getInstance(classOf[ActorMaterializer])
 
-  val r = injector.getInstance(classOf[RootRoute])
-  val config = injector.getInstance(classOf[Config])
-
-  Http().bindAndHandle(r(), config.getString("http.interface"), config.getInt("http.port"))
-
+  implicit val cluster = Cluster(system)
+  //  val r = injector.getInstance(classOf[RootRoute])
+  //  val config = injector.getInstance(classOf[Config])
+  //
+  //  Http().bindAndHandle(r(), config.getString("http.interface"), config.getInt("http.port"))
   println(logo)
 
   lazy val logo = """
@@ -30,4 +31,6 @@ object Main extends App {
    / _, _/ /_/ / /_/ / /_  
   /_/ |_|\____/\____/\__/  """
 
+  val paths = Seq()
+  system.actorOf(Props(new RootRouter(collection.immutable.Seq(paths: _*))))
 }
--- root/src/main/scala/io/upblockchain/root/modules/ServiceModule.scala
+++ root/src/main/scala/io/upblockchain/root/modules/ServiceModule.scala
@@ -2,11 +2,10 @@ package io.upblockchain.root.modules
 
 import io.upblockchain.common.modules.BaseModule
 import com.google.inject.{ Provides, Singleton }
-import akka.actor.ActorSystem
+import akka.actor.{ ActorRef, ActorSystem, Props }
 import akka.stream.ActorMaterializer
 import javax.inject.Inject
 import io.upblockchain.root.services.EthJsonRPCService
-import akka.actor.ActorRef
 import javax.inject.Named
 import akka.cluster.client.ClusterClient
 import akka.cluster.client.ClusterClientSettings
@@ -23,7 +22,7 @@ trait ServiceModule extends BaseModule {
   def provideConfig: Config = ConfigFactory load
 
   @Provides @Singleton
-  def provideActorSystem: ActorSystem = ActorSystem("RootSystem")
+  def provideActorSystem(@Inject() config: Config): ActorSystem = ActorSystem("ClusterSystem", config)
 
   @Provides @Singleton
   def provideActorMaterializer(@Inject() sys: ActorSystem): ActorMaterializer = ActorMaterializer()(sys)
@@ -32,11 +31,6 @@ trait ServiceModule extends BaseModule {
   def provideClusterPros(@Inject() sys: ActorSystem): ActorRef = {
     sys.actorOf(ClusterClient.props(ClusterClientSettings(sys)), "cluster_client")
   }
-
-  //  @Provides @Singleton @Named("SimpleClusterListener")
-  //  def provideClusterListener(@Inject() sys: ActorSystem): ActorRef = {
-  //    sys.actorOf(Props[SimpleClusterListener], "SimpleClusterListener")
-  //  }
 
 }
 
