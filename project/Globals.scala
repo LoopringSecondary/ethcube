9756dbe55a36c1060b13f631bad14f469b318e34
wangdong <wangdong@zhongan.io>
2018-09-11 16:34:27 -0400

    fix version

--- project/Globals.scala
+++ project/Globals.scala
@@ -1,6 +1,6 @@
 object Globals {
   lazy val name = "ethcube"
-  lazy val version = "0.0.1"
+  lazy val version = "0.0.2-SNAPSHOT"
   lazy val scalaVersion = "2.12.6"
   lazy val organizationName = "loopring Inc."
   lazy val organization = "org.loopring"
