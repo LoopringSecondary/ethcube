6c4ba7d5c34bf6d59176e07cbc53a204883209fc
Daniel Wang <dong77@gmail.com>
2018-09-13 17:49:57 -0400

    fix publishing issue (#22)

--- project/Globals.scala
+++ project/Globals.scala
@@ -1,6 +1,5 @@
 object Globals {
   lazy val name = "ethcube"
-  lazy val version = "0.0.2-SNAPSHOT"
   lazy val scalaVersion = "2.12.6"
   lazy val organizationName = "loopring Inc."
   lazy val organization = "org.loopring"
